a1e913644b9dae818692f0f947109e4a0e494cdb
kevinazevedo <taylorvision95@outlook.com>
2024-11-01 04:08:21 -0300

    Tamanhos randômicos

--- src/js/game.js
+++ src/js/game.js
@@ -11,8 +11,8 @@ function ajustaTamanhoPalcoJogo() {
 ajustaTamanhoPalcoJogo();
 
 function posicaoRandomica() {
-    var posicaoX = Math.floor(Math.random() * largura) - 90;
-    var posicaoY = Math.floor(Math.random() * altura) - 90;
+    var posicaoX = Math.floor(Math.random() * largura) - 100;
+    var posicaoY = Math.floor(Math.random() * altura) - 100;
 
     posicaoX = posicaoX < 0 ? 0 : posicaoX;
     posicaoY = posicaoY < 0 ? 0 : posicaoY;
@@ -22,10 +22,29 @@ function posicaoRandomica() {
     // criar o elemento HTML
     var mosquito = document.createElement("img");
     mosquito.src = "img/mosquito.png";
-    mosquito.className = "mosquito1";
+    mosquito.className = tamanhoAleatorio();
     mosquito.style.left = posicaoX + "px";
     mosquito.style.top = posicaoY + "px";
     mosquito.style.position = "absolute";
 
     document.body.appendChild(mosquito);
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 4);
+    
+    switch(classe) {
+        case 0:
+            return "mosquito1"
+        ;
+        case 1:
+            return "mosquito2"
+        ;
+        case 2:
+            return "mosquito3"
+        ;
+        case 3:
+            return "mosquito4"
+        ;
+    }
 }
