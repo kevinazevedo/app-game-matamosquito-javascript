ec716d68362ff4977ea6a5ad922205da2bcba5fb
kevinazevedo <taylorvision95@outlook.com>
2024-11-01 18:43:18 -0300

    Controlando os pontos de vida

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,6 @@
 var altura = 0;
 var largura = 0;
+var vidas = 1;
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight;
@@ -14,6 +15,16 @@ function posicaoRandomica() {
     // remover o mosquito anterior (caso exista)
     if (document.getElementById("mosquito") != null) {
         document.getElementById("mosquito").remove();
+
+        if(vidas>3) {
+            alert("Interromper o jogo (game over)");
+        }
+
+        else {
+            document.getElementById("v" + vidas).src="../img/coracao_vazio.png";
+
+            vidas++;
+        }
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 100;
@@ -32,6 +43,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + "px";
     mosquito.style.position = "absolute";
     mosquito.id = "mosquito";
+    mosquito.onclick = function() {
+        this.remove();
+    }
 
     document.body.appendChild(mosquito);
 }
